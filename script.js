283daba8791847ca6c7539d816d4f51c327607d0
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-03 15:20:07 +0100

    starting files fixed

--- script.js
+++ script.js
@@ -1,48 +1,19 @@
 /*
 Consegna
-L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero (in ordine) tra quelli compresi in un range:
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
-Lasciate perdere il cambiamento del colore della cella (serve una cosa che oggi non abbiamo visto). Qundi le celle saranno tutte dello stesso colore e non succede niente quando le clicchiamo.
-Consigli del giorno:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+BONUS:
+1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
 */
-
-
-// let container = document.querySelector(`main`);
-// let grid = document.createElement(`div`);
-// grid.className = "grid";
-// container.append(grid);
-
-// const btnPlay = document.getElementById(`play`);
-
-// btnPlay.addEventListener('click', play);
-
-// function play() {
-//     const selection = document.getElementById(`difficulty`);
-    
-//     if (selection.value == `difficulty_1`) {
-//         const grid1 = generateListNumsBetween(1, 100);
-//     } else if (selection.value == `difficulty_2`) {
-//         const grid2 = generateListNumsBetween(1, 81);
-//     } else {
-//         const grid3 = generateListNumsBetween(1, 49);
-//     }
-
-//     function generateListNumsBetween(min, max) {
-//         grid.innerHTML = ``;
-
-//         for (let min = 1; min <= max; min++) {
-//             grid.innerHTML += `<div class="box box-${max}">${min}</div>`;
-//         }
-
-//     }
-// }
 
 
 const eleLevel = document.getElementById(`level`);
