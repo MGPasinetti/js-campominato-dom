dcba685108e1ccbd3b37253a17e570a4171373eb
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-03 15:44:47 +0100

    fixing details

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
 Consegna
-Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco.
 L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
@@ -36,23 +36,19 @@ function setupGame() {
     for (let cellNum = 1; cellNum <= cellsCount; cellNum++) {
         const eleCell = document.createElement(`div`);
         eleCell.classList.add(`cell`);
-        // eleCell.append(cellNum); // metodo 1
-        eleCell.innerHTML = cellNum; // metodo 2
+        eleCell.innerHTML = cellNum;
         eleCell.style.width = `calc(100% / ${cellsPerRow})`;
         eleCell.style.height = `calc(100% / ${cellsPerRow})`;
-        eleCell.addEventListener(`click`, changeCellColor); // TODO: 
+        eleCell.addEventListener(`click`, changeCellColor); 
         eleGrid.append(eleCell);
 
         console.log(cellNum);
     }   
-    // stampare la griglia in base al livello
 
-    // settare la logica del gioco
+    // settare la logica del gioco:
 }
 
 function changeCellColor(event) {
-    // this.classList.add(`selected`);
-    this.classList.toggle(`selected`);
+    this.classList.add(`selected`);
 
-    // console.log(event.target);
 }
